06862a611058c855844c01d097928ec602d71e08
Roberto Antoniello <robertino.antoniello@gmail.com>
2023-02-01 09:25:42 +0000

    Update cluster info

--- src/main.tf
+++ src/main.tf
@@ -9,19 +9,11 @@ resource "azurerm_kubernetes_cluster" "k8s" {
     node_count = 3
     vm_size    = "Standard_DS2_v2"
     max_pods   = 110
+    enable_auto_scaling = true
+    min_count = 3
+    max_count = 6
   }
   identity {
     type = "SystemAssigned"
   }
-}
-
-#Definizione autoscale Azure
-resource "azurerm_kubernetes_cluster_node_pool" "autoscale" {
-  name                  = "k8sautoscale"
-  kubernetes_cluster_id = azurerm_kubernetes_cluster.k8s.id
-  vm_size               = "Standard_DS2_v2"
-  enable_auto_scaling   = true
-  node_count            = 3
-  min_count             = 3
-  max_count             = 6
 }
