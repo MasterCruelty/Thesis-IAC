dbb9e6fa5d36e52a33c4b440a026fbad3c4a86cf
Roberto antoniello <robertino.antoniello@gmail.com>
2023-04-05 17:46:29 +0200

    update utility

--- script_vari/utility.sh
+++ script_vari/utility.sh
@@ -9,3 +9,8 @@ cat ~/.docker/config.json | base64 -w0
 #Applicare il manifesto yaml(presente in questo path) e creare il segreto
 #Essenzialmente la stessa procedura fatta sopra ma dando in pasto lo yaml invece di farlo a mano
 kubectl create -f secret.yaml
+
+#creare private e public pem
+openssl genrsa -out keypair.pem 2048
+openssl rsa -in keypair.pem -pubout -out public.pem
+openssl pkcs8 -topk8 -inform PEM -outform PEM -nocrypt -in keypair.pem -out private.pem
